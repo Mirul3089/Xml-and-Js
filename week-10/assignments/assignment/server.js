2466f9ce71344ebfa04395a3e9826a99b79f2fec
Mirul3089 <55869588+Mirul3089@users.noreply.github.com>
2022-03-22 22:17:35 -0400

    Assignment submission

---
+++ week-10/assignments/assignment/server.js
@@ -0,0 +1,35 @@
+const http = require("http");
+
+const employee = require("./assignment.json");
+
+const parseURLParams = (paramsString) => {
+    const params = new URLSearchParams(paramsString);
+
+    return Array.from(params.entries()).reduce((acc, [key, value]) => ({ ...acc, [key]: value}), {})
+}
+
+const server = http.createServer(async(req, res) => {
+
+    const [path, paramsString] = req.url.split("?");
+
+    if( path === "/api/assignment") {
+        const params = parseURLParams(paramsString);
+        const { code, data} = await employee.getAll(params);
+
+        res.writeHead(code, { "Content-Type": "application/json" });
+        res.end(data);
+    } else if(path.match(/\/api\/assignment\/\w+/)) {
+         const id = path.split("/")[3];
+
+         const { code, data } = await employee.getById(id);
+
+         res.writeHead(code, {"Content-Type" : "application/json"});
+         res.end(data);
+    } else {
+        res.writeHead(404,{"Content-Type" : "application/json"} )
+        res.end('No router found!')
+    }
+    
+})
+
+server.listen(8888);
